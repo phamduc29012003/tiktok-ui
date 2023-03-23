65c15273d607bd755058b69cfbfd6d6847464552
phamduc29012003 <94993317+phamduc29012003@users.noreply.github.com>
2023-03-23 15:34:00 +0700

    Router for websize

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+//Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+import Home from '~/Pages/Home';
+import Following from '~/Pages/Following';
+import Profile from '~/Pages/Profile';
+import Upload from '~/Pages/Upload';
+import Search from '~/Pages/Search';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { privateRoutes, publicRoutes };
